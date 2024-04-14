5473e9cbfae0c1280c944759c09bb8ba807b0844
Ady <adysyukri@gmail.com>
2024-04-14 13:19:05 +0800

    chore: added tailwind to base layout

--- main.go
+++ main.go
@@ -8,9 +8,13 @@ import (
 )
 
 func main() {
-	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("GET /home", func(w http.ResponseWriter, r *http.Request) {
 		page.Home().Render(r.Context(), w)
 	})
+
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("public/"))))
 
 	fmt.Println("Listening on localhost:3000")
 	http.ListenAndServe(":3000", nil)
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./templates/**/*.{html,js,templ}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+    ],
+}
