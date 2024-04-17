2207d70981259b707d181a9aebfb88b5e09dc8e6
Ady <adysyukri@gmail.com>
2024-04-17 15:33:19 +0800

    chore: added routes and logic to each route

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/adysyukri/bookemarker-go/internal/bookmark"
+	"github.com/adysyukri/bookemarker-go/pkg/sqlite"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -12,8 +15,64 @@ var db, _ = sql.Open("sqlite3", "./tmp/db.db")
 
 func main() {
 	defer db.Close()
+
+	dbClient := sqlite.NewClient(db)
+	svc := bookmark.NewService(*dbClient)
+
 	http.HandleFunc("GET /home", func(w http.ResponseWriter, r *http.Request) {
-		//page.Home().Render(r.Context(), w)
+		t, err := svc.Get(r.Context())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "error occurs: %s", err)
+			return
+		}
+
+		t.Render(r.Context(), w)
+	})
+
+	http.HandleFunc("POST /add", func(w http.ResponseWriter, r *http.Request) {
+		total, err := strconv.Atoi(r.FormValue("total"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "error occurs: %s", err)
+			return
+		}
+
+		read, err := strconv.Atoi(r.FormValue("read"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "error occurs: %s", err)
+			return
+		}
+
+		bp := &bookmark.BookmarkParams{
+			Title:  r.FormValue("title"),
+			Author: r.FormValue("author"),
+			Total:  total,
+			Read:   read,
+		}
+
+		t, err := svc.Add(r.Context(), bp)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "error occurs: %s", err)
+			return
+		}
+
+		t.Render(r.Context(), w)
+	})
+
+	http.HandleFunc("DELETE /delete/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		// w.Write([]byte(id))
+		err := svc.Delete(r.Context(), id)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "error occurs: %s", err)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	})
 
 	http.Handle("/static/",
